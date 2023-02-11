6bfebb88fa1e5a79533262e5d2ec690119f09e1a
marcellusc <96213317+marcellusc@users.noreply.github.com>
2023-02-11 00:20:05 +0000

    INIT

---
+++ wp-content/plugins/tidio-live-chat/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b9082646d1ebff969b31c013c6f0a625637e9eae',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b9082646d1ebff969b31c013c6f0a625637e9eae',
+    ),
+  ),
+);
